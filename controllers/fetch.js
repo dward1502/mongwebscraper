d053570582665bb6a78764fd144f5d5fb47e10db
dward1502@gmail.com <dward1502@gmail.com>
2018-03-19 12:40:02 -0700

    got scraper to work

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -3,30 +3,53 @@ const mongoose = require('mongoose');
 const cheerio = require('cheerio');
 const db = require("../models");
 const axios = require('axios');
+const request = require('request');
 
 const app = express();
 
-app.get("/scrape", (req,res)=>{
-   axios.get("https://www.leafly.com/news").then((response)=>{
-      var $ = cheerio.load(response.data);
+// app.get("/scrape", (req,res)=>{
+//    axios("https://www.leafly.com/news", function(response,html,error){
+    
+//         var $ = cheerio.load(html);
+//         $('span.leafly-title').each(function (i, element) {
 
-      $("header ").each((i,element)=>{
-         let result = [];
+//             var title = $(this).text();
+//             var url = $(this).parent().attr('href');
+//             var data = {         
+//                 title: title,
+//                 url: url             
+//             };
+//             console.log(data);
+//             db.Headline.create(data).then((dbArticle) => {
+//             console.log(dbArticle);            
+//             }).catch((err)=>{
+//                 return res.json(err);
+//             });         
+//         });
+//            res.send("Scrape Complete");
          
-         result.title = $(this).children("h2").text();
-         result.link = $(this).children("a").attr("href");
-         
-         db.Headline.create(result).then((dbArticle)=>{
+//    });
+// });
+request.get("https://www.leafly.com/news", function (error, response, html, res) {
+    if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        $('span.leafly-title').each(function (i, element) {
+
+            var title = $(this).text();
+            var url = $(this).parent().attr('href');
+            var data = {         
+                title: title,
+                url: url             
+            };
+            console.log(data);
+            db.Headline.create(data).then((dbArticle,res) => {
             console.log(dbArticle);            
-         }).catch((err)=>{
-            return res.json(err);
-         });
-         console.log(result);
-         
-      });
-      
-      res.send("Scrape Complete");
-   });
+            }).catch((err)=>{
+                
+            });         
+        });
+       
+    }
 });
 module.exports = app;
 
